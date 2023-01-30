0560bbae96fa7b4096d58bda059dff83068f76cb
Jean-Christian Denis <contact@jcdenis.fr>
2023-01-30 21:20:49 +0100

    add translation, thx @brol, fix #1

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,14 @@
+<?php
+// Language: Français
+// Module: tinyPacker - 
+// Date: 2023-01-30 18:50:34
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Destination directory is not writable.'] = 'Le répertoire de destination n\'est pas accessible en écriture.';
+l10n::$locales['Quick pack theme or plugin into public dir'] = 'Empaquetage rapide de thème ou plugin dans le répertoire public';
